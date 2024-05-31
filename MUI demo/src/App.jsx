dfdd6fad7d533e9c68cd219ff02e988e72ed246c
aakashstha1 <aakash.cerestha90@gmail.com>
2024-05-31 22:04:33 +0545

    fourth commit

--- MUI demo/src/App.jsx
+++ MUI demo/src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import { MuiBtnGroup } from "./components/MuiBtnGroup";
 // import { MuiTypography } from "./components/MuiTypography";
 // import { MuiButton } from "./components/MuiButton";
 // import { MuiBtnSize } from "./components/MuiBtnSize";
@@ -10,8 +9,7 @@ function App() {
       {/* <MuiTypography />
       <MuiButton />
       <MuiBtnSize /> */}
-      <MuiIconBtn/>
-      <MuiBtnGroup/>
+      <MuiIconBtn />
     </>
   );
 }
--- MUI demo/src/components/MuiIconBtn.jsx
+++ MUI demo/src/components/MuiIconBtn.jsx
@@ -1,24 +1,51 @@
-import { Stack, Button, IconButton } from "@mui/material";
+import { Stack, Button, IconButton, ButtonGroup } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 export const MuiIconBtn = () => {
   return (
-    <Stack display={"block"} spacing={2} direction={"row"}>
-      <Button
-        variant="contained"
-        startIcon={<SendIcon />}
-        disableRipple
-        onClick={() => {
-          alert("Clicked");
-        }}
-      >
-        Send
-      </Button>
-      <Button variant="contained" endIcon={<SendIcon />} disableElevation>
-        Send
-      </Button>
-      <IconButton aria-label="send" color="success" size="medium">
-        <SendIcon />
-      </IconButton>
+    <Stack spacing={5}>
+      <Stack display={"block"} spacing={2} direction={"row"}>
+        <Button
+          variant="contained"
+          startIcon={<SendIcon />}
+          disableRipple
+          onClick={() => {
+            alert("Clicked");
+          }}
+        >
+          Send
+        </Button>
+        <Button variant="contained" endIcon={<SendIcon />} disableElevation>
+          Send
+        </Button>
+        <IconButton aria-label="send" color="success" size="medium">
+          <SendIcon />
+        </IconButton>
+      </Stack>
+      <Stack direction={"row"} spacing={3}>
+        <ButtonGroup variant="text" orientation="vertical">
+          <Button variant="outlined">Left</Button>
+          <Button variant="outlined">Center</Button>
+          <Button variant="outlined">Right</Button>
+        </ButtonGroup>
+      </Stack>
+      <Stack direction={"row"} spacing={3}>
+        <ButtonGroup
+          variant="contained"
+          orientation="vertical"
+          color="secondary"
+          aria-label="allignment button group"
+        >
+          <Button
+            onClick={() => {
+              alert("Clicked Left");
+            }}
+          >
+            Left
+          </Button>
+          <Button>Center</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
+      </Stack>
     </Stack>
   );
 };
